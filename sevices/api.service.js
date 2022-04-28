e0883c95f2a424c43c00a5a211ff64e4348d5d07
Aliaksandr Senkevich <puskin1914@gmail.com>
2022-04-28 15:04:05 +0300

    refactoring getWeather

--- sevices/api.service.js
+++ sevices/api.service.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import https from 'https';
 import { getValue, TOKEN_DICTIONARY } from './storage.service.js';
 
@@ -8,22 +9,20 @@ const getWeather = async (city) => {
       'Не задан ключ API, задайте его через команду weather -t [API_KEY]'
     );
   }
-  const url = new URL(`https://api.openweathermap.org/data/2.5/weather`);
-  url.searchParams.append('q', city);
-  url.searchParams.append('appid', token);
-  url.searchParams.append('lang', 'ru');
-  url.searchParams.append('units', 'metric');
 
-  https.get(url, (response) => {
-    let res = '';
-    response.on('data', (chunk) => {
-      res += chunk;
-    });
+  const { data } = await axios.get(
+    'https://api.openweathermap.org/data/2.5/weather',
+    {
+      params: {
+        q: city,
+        appid: token,
+        lang: 'ru',
+        units: 'metric',
+      },
+    }
+  );
 
-    response.on('end', () => {
-      console.log(res);
-    });
-  });
+  return data;
 };
 
 export { getWeather };
--- weather.js
+++ weather.js
@@ -33,6 +33,7 @@ const initCLI = () => {
   }
 
   getWeather('minsk');
+  // Вывод погоды
 };
 
 initCLI();
